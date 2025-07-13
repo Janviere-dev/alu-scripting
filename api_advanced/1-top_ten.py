432d1183b4f22def72a90c765ce792bd5b3a5cc1
Janviere-dev <j.munezero@alustudent.com>
2025-07-13 14:28:40 -0700

    first 10 hot posts for a subreddit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""Module to print titles of top 10 hot posts from a subreddit"""
+"""
+1-top_ten module:
+Fetches and prints titles of first 10 hot posts for a subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints titles of the first 10 hot posts from a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        "User-Agent": "python:top.ten:v1.0 (by /u/alumunezero)"
-    }
-    params = {"limit": 10}
+    """
+    Prints titles of the first 10 hot posts for a given subreddit.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'alu-scripting:v1.0 (by /u/fakebot)'}
 
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-        if response.status_code != 200:
-            print(None)
-            return
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        posts = response.json().get("data", {}).get("children", [])
-        for post in posts:
-            title = post.get("data", {}).get("title")
-            if title:
-                print(title)
-    except requests.RequestException:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get('data', {}).get('children', [])
+
+    for post in data:
+        print(post['data'].get('title'))
