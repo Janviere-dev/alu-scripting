6eb7df874bf2e597e95bdb8d56154d46b720a564
Janviere-dev <j.munezero@alustudent.com>
2025-07-13 14:32:25 -0700

    first 10 hot posts for a subreddit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-1-top_ten module:
-Fetches and prints titles of first 10 hot posts for a subreddit.
+This module queries the Reddit API and prints the titles of the
+first 10 hot posts for a given subreddit using the Requests library.
+If the subreddit is invalid, it prints None.
 """
+
 
 import requests
 
